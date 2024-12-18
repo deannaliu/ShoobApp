08ef84ed23c1f26c5840da182358544f090d1719
deannaliu <deanna.mei.liu@gmail.com>
2024-12-17 19:43:35 -0800

    Initial Commit

---
+++ ShoobApp/ShoobAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoobAppApp.swift
+//  ShoobApp
+//
+//  Created by Deanna Liu on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoobAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoobAppTests/ShoobAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoobAppTests.swift
+//  ShoobAppTests
+//
+//  Created by Deanna Liu on 12/17/24.
+//
+
+import Testing
+@testable import ShoobApp
+
+struct ShoobAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
